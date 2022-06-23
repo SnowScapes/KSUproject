6f584beec76b37d51063a14e71b999c8071a361d
SnowScapes <39547945+SnowScapes@users.noreply.github.com>
2022-06-23 11:41:38 +0900

    KSOOP 0405

---
+++ 0405-2/0405-2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0405_2
+{
+    class Country
+    {
+        public string Address = "Korea, Republic of | ";
+        public Country()
+        {
+            Console.WriteLine("Country 부모 클래스 생성");
+        }
+    }
+    class City : Country
+    {
+        public City(String city)
+        {
+            this.Address += city;
+            Console.WriteLine("City 자식 클래스 생성");
+        }
+        public void ShowAddress()
+        {
+            Console.WriteLine(Address);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            City c = new City("Busan");
+            c.ShowAddress();
+        }
+    }
+}
