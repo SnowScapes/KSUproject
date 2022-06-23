17b79caa8356a3a9715afc984c1f0f537e6259ba
SnowScapes <39547945+SnowScapes@users.noreply.github.com>
2022-06-23 11:40:24 +0900

    KSOOP 0322

---
+++ 0322/0322/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0322
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 10 )
+            {
+                i++;
+                if (i % 2 == 0)
+                    continue;
+                Console.WriteLine(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
