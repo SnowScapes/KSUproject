c38e91043db33f8f5e98e16cb7e91bd62d22dcfc
SnowScapes <39547945+SnowScapes@users.noreply.github.com>
2022-06-23 11:41:09 +0900

    KSOOP 0405

---
+++ 0405/0405/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0405
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 6; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 0; j < 2*i+1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
