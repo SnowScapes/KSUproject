ae5109d5300124c053ca4f9d6eb0b2a0034006bd
SnowScapes <39547945+SnowScapes@users.noreply.github.com>
2022-06-23 11:39:06 +0900

    KSOOP 0315

---
+++ 0315/0315/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0315
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = int.Parse(Console.ReadLine());
+            if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+                Console.WriteLine("윤년");
+            else
+                Console.WriteLine("평년");
+        }
+    }
+}
