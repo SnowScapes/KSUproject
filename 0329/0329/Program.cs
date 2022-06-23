e85df4c730d0edfd64868c6ac5c6d689075bb98f
SnowScapes <39547945+SnowScapes@users.noreply.github.com>
2022-06-23 11:40:47 +0900

    KSOOP 0329

---
+++ 0329/0329/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0329
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[args.Length];
+            for (int i = 0; i < args.Length; i++)
+            {
+                n[i] = int.Parse(args[i]);
+            }
+            SumAvg(n);
+        }
+        static void SumAvg(int[] n)
+        {
+            int sum = 0;
+            for (int i = 0; i < n.Length; i++)
+            {
+                sum += n[i];
+            }
+            int avg = sum / n.Length;
+            Console.WriteLine("합 : {0}\n평균 : {1}", sum, avg);
+        }
+    }
+}
